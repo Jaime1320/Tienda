86bf0c11d920ff73477b237fb0f74d0e2ab7c201
Jaime1320 <jhc0001@alu.medac.es>
2023-11-08 12:15:09 +0100

    tienda

--- principal.php
+++ principal.php
@@ -7,6 +7,8 @@
     <title>Iniciar sesion</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <?php require "BaseDatos/base_tienda.php" ?>
+    <?php require 'Objetos/producto.php'; ?>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -15,17 +17,71 @@
 
     if (isset($_SESSION["usuario"])) {
         $usuario = $_SESSION["usuario"];
-    }else{
+    } else {
         //header("Location: iniciarsesion.php");
         $_SESSION["usuario"] = "invitado";
         $usuario = $_SESSION["usuario"];
     }
+
+    $sql = "SELECT * from productos";
+    $resultado = $conexion->query($sql);
+    $productos = [];
+
+    while ($fila = $resultado->fetch_assoc()) {
+        $nuevo_producto = new Producto(
+            $fila["idProducto"],
+            $fila["nombreProducto"],
+            $fila["precio"],
+            $fila["descripcion"],
+            $fila["cantidad"],
+            $fila["imagen"]
+        );
+        array_push($productos, $nuevo_producto);
+    }
+
     ?>
     <div class="container">
         <h1>Pagina principal</h1>
-        <h2>Bienvenid@ <?php echo $usuario?></h2>
-        
+        <h2>Bienvenid@ <?php echo $usuario ?></h2>
+
+    </div>
+    <div class="container">
+        <table class="table table-striped table-hover">
+            <thead class="table table-dark">
+                <tr>
+                    <th>ID Producto</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Descripción</th>
+                    <th>Cantidad</th>
+                    <th>Imagen</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM productos";
+                $resultado = $conexion->query($sql);
+
+                foreach ($productos as $producto) {
+                    echo "<tr>";
+                    echo "<td>" . $producto->idProducto . "</td>";
+                    echo "<td>" . $producto->nombreProducto . "</td>";
+                    echo "<td>" . $producto->precio . "</td>";
+                    echo "<td>" . $producto->descripcion . "</td>";
+                    echo "<td>" . $producto->cantidad . "</td>";
+
+                ?>
+                    <td>
+                        <img witdh="50" height="100" src="<?php echo $producto->imagen ?>">
+                    </td>
+                <?php
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- productos.php
+++ productos.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Productos</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <?php require '../Tienda/BaseDatos/base_tienda.php' ?>
 </head>
 
 <body>
@@ -24,6 +25,13 @@
         $temp_descripcion = depurar($_POST["descripcion"]);
         $temp_cantidad = depurar($_POST["cantidad"]);
 
+        //$_FILES["nombreCampo"]["queQueremosCoger"] -> TYPE, NAME, SIZE, TMP_NAME
+        $nombre_imagen = $_FILES["imagen"]["name"];
+        $tipo_imagen = $_FILES["imagen"]["type"];
+        $tamano_imagen = $_FILES["imagen"]["size"];
+        $ruta_temporal = $_FILES["imagen"]["tmp_name"];
+        //echo $nombre_imagen . " " . $tipo_imagen . " " . $tamano_imagen . " " . $ruta_temporal;
+
 
         #   Validación nombreProducto
         if (strlen($temp_nombreProducto) == 0) {
@@ -34,7 +42,6 @@
                 $err_nombreProducto = "El nombre tiene que tener entre 1 y 40 caracteres";
             } else {
                 $nombreProducto = $temp_nombreProducto;
-                echo $nombreProducto;
             }
         }
 
@@ -47,7 +54,6 @@
                 $err_precio = "El precio tiene que tener entre 1 y 10 caracteres";
             } else {
                 $precio = $temp_precio;
-                echo $precio;
             }
         }
 
@@ -55,15 +61,12 @@
         if (strlen($temp_descripcion) == 0) {
             $err_descripcion = "La descripcion es obligatoria";
         } else {
-            $patron = "/^[a-zA-Z0-9]{1,40}$/";
-            if (!preg_match($patron, $temp_descripcion)) {
-                $err_descripcion = "La descripcion tiene que tener entre 1 y 40 caracteres";
+            if (strlen($temp_descripcion) > 255) {
+                $err_descripcion = "La descripcion no puede tener mas de 255 caracteres";
             } else {
                 $descripcion = $temp_descripcion;
-                echo $descripcion;
             }
         }
-
         #  Validación cantidad
         if (strlen($temp_cantidad) == 0) {
             $err_cantidad = "La cantidad es obligatoria";
@@ -73,7 +76,17 @@
                 $err_cantidad = "La cantidad tiene que tener entre 1 y 10 caracteres";
             } else {
                 $cantidad = $temp_cantidad;
-                echo $cantidad;
+            }
+        }
+        #   Validación imagen
+        if ($tamano_imagen > 10000000) {
+            $err_imagen = "La imagen no puede pesar mas de 1MB";
+        } else {
+            if ($tipo_imagen != "image/jpeg") {
+                $err_imagen = "Tiene que ser formato imagen";
+            } else {
+                $ruta_final = "imagenes/" . $nombre_imagen;
+                move_uploaded_file($ruta_temporal, $ruta_final);
             }
         }
     }
@@ -82,35 +95,46 @@
     <div class="container">
         <h1>Insertar producto</h1>
         <div>
-            <form action="" method="post">
+            <form action="" method="post" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label class="form-label">Nombre producto: </label>
                     <input class="form-control" type="text" name="nombreProducto">
-                    <?php if (isset ($err_nombreProducto)) echo '<label class=text-danger>'.$err_nombreProducto. '</label>' ?>
+                    <?php if (isset($err_nombreProducto)) echo '<label class=text-danger>' . $err_nombreProducto . '</label>' ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Precio: </label>
-                    <input class="form-control" type="float" name="precio">
+                    <input class="form-control" type="text" name="precio">
                     <?php if (isset($err_precio)) echo '<label class=text-danger>' . $err_precio . '</label>' ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Descripción: </label>
                     <input class="form-control" type="text" name="descripcion">
-                    <?php if (isset ($err_descripcion)) echo '<label class=text-danger>'.$err_descripcion. '</label>' ?>
+                    <?php if (isset($err_descripcion)) echo '<label class=text-danger>' . $err_descripcion . '</label>' ?>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Cantidad: </label>
-                    <input class="form-control" type="int" name="cantidad">
-                    <?php if (isset ($err_cantidad)) echo '<label class=text-danger>'.$err_cantidad. '</label>' ?>
+                    <input class="form-control" type="text" name="cantidad">
+                    <?php if (isset($err_cantidad)) echo '<label class=text-danger>' . $err_cantidad . '</label>' ?>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Imagen</label>
+                    <input class="form-control" type="file" name="imagen">
+                    <?php if (isset($err_imagen)) echo '<label class=text-danger>' . $err_imagen . '</label>' ?>
                 </div>
                 <button class="btn btn-primary" type="submit">Enviar</button>
             </form>
         </div>
     </div>
     <?php
-    if (isset($nombreProducto) && isset($precio) && isset($descripcion) && isset($cantidad)) {
-        $sql = "INSERT INTO productos (nombreProducto, precio, descripcion, cantidad) VALUES ('$nombreProducto', '$precio', '$descripcion', '$cantidad')";
+    if (isset($nombreProducto) && isset($precio) && isset($descripcion) && isset($cantidad) && isset($ruta_final)) {
+        $sql = "INSERT INTO productos (nombreProducto, precio, descripcion, cantidad , imagen)
+        VALUES ('$nombreProducto',
+        '$precio',
+        '$descripcion',
+        '$cantidad',
+        '$ruta_final')";
         $conexion->query($sql);
+        echo "<div class='container'><h3>Producto insertado con éxito</h3></div>";
     }
     ?>
 </body>
